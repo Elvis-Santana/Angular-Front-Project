f5d466e2b33b971e49c08dc0e20e51407506b5f6
Elvis Santana <elvissantanaguimaraes@gmail.com>
2023-11-23 09:25:04 -0300

    Criando repositório no Github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular-course-se-carsales';
+  title = 'Front em Angular';
 }
